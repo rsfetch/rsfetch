bb4df4d9bb9ce3ef2110e4340b8f8f2b224997b1
Phate6660 <afauacakagaoaoagalae@gmail.com>
2019-04-26 07:20:16 -0400

    Updated main.rs and screenshot.

--- main.rs
+++ main.rs
@@ -11,6 +11,11 @@ fn main() {
 					.arg("grep $USER /etc/passwd | sed 's/.*://'")
 					.output()
 					.expect("failed to execute process");
+	let uptime = Command::new("/usr/bin/bash")
+					.arg("-c")
+					.arg("uptime -p | sed 's/up //'")
+					.output()
+					.expect("failed to execute process");
 	let distro = Command::new("/usr/bin/bash")
 					.arg("-c")
 					.arg("grep PRETTY /etc/os-release | grep -o '\".*\"' | sed 's/\"//g' | awk 'NR>1{print PREV} {PREV=$0} END{printf(\"%s\",$0)}'")
@@ -25,21 +30,24 @@ fn main() {
 					.arg("tail -n 1 \"${HOME}/.xinitrc\" | cut -d ' ' -f 2")
 					.output()
 					.expect("failed to execute process");
-	// Variables for package counts.
 	let pkgs = Command::new("/usr/bin/bash")
 					.arg("-c")
 					.arg("echo \"$(pacman -Q | wc -l) (total) | $(paclist core | wc -l) (core), $(paclist extra | wc -l) (extra), $(paclist community | wc -l) (community), $(pacman -Qm | wc -l) (aur)\"")
 					.output()
 					.expect("failed to execute process");
 	// Output
-	println!("\\    / /\\   |    |    |--- \\   /     |");
-	println!(" \\  / /__\\  |    |    |---  \\ /      |");
-	println!("  \\/ /----\\ |___ |___ |---   |       |");
-	println!("--------------------------------------");
-	print!("USER             =   {}", String::from_utf8_lossy(&user.stdout));
-	print!("SHELL            =   {}", String::from_utf8_lossy(&shell.stdout));
-	print!("WINDOW MANAGER   =   {}", String::from_utf8_lossy(&wm.stdout));
-	println!("DISTRO           =   {}", String::from_utf8_lossy(&distro.stdout));
-	print!("KERNEL           =   {}", String::from_utf8_lossy(&kernel.stdout));
-	print!("PACKAGES         =   {}", String::from_utf8_lossy(&pkgs.stdout));
+	println!("");
+	println!("-------------------------------------------");
+	println!("|    \\    / /\\   |    |    |--- \\   /     |");
+	println!("|     \\  / /__\\  |    |    |---  \\ /      |");
+	println!("|      \\/ /----\\ |___ |___ |---   |       |");
+	println!("-------------------------------------------");
+	print!("USER                =   {}", String::from_utf8_lossy(&user.stdout));
+	print!("SHELL               =   {}", String::from_utf8_lossy(&shell.stdout));
+	print!("WINDOW MANAGER      =   {}", String::from_utf8_lossy(&wm.stdout));
+	println!("DISTRO              =   {}", String::from_utf8_lossy(&distro.stdout));
+	print!("KERNEL              =   {}", String::from_utf8_lossy(&kernel.stdout));
+	print!("UPTIME              =   {}", String::from_utf8_lossy(&uptime.stdout));
+	print!("PACKAGES            =   {}", String::from_utf8_lossy(&pkgs.stdout));
+	println!("");
 }
