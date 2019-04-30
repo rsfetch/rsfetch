1b5a00743831966ffa74c0d81ba6e3b331ef30c4
Phate6660 <afauacakagaoaoagalae@gmail.com>
2019-04-29 22:09:23 -0400

    Added option to display default editor.

--- src/main.rs
+++ src/main.rs
@@ -22,73 +22,79 @@ fn main() {
 						.build();
 	table.set_format(format);
 	let matches = App::new("fetch")
-					.version("0.9.5")
+					.version("0.9.6")
 					.about("\nFetches system info. Somewhat(?) minimalistic.\nAll \"BOOL\" options default to \"true\" (with the exception of separate package counts), and \"SOURCE\" defaults to no.\n\nNote: If you set -P to \"true\", make sure to set -p to \"false\".")
 					.arg(Arg::with_name("caps")
 						.short("c")
 						.long("caps")
 						.value_name("BOOL")
-						.help("Turn all caps on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn all caps on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("user")
 						.short("U")
 						.long("user")
 						.value_name("BOOL")
-						.help("Turn user name on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn user name on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("ip_address")
 						.short("i")
 						.long("ip_address")
 						.value_name("BOOL")
-						.help("Turn ip address display on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn ip address display on or off.")
+						.takes_value(true))
+					.arg(Arg::with_name("editor")
+						.short("e")
+						.long("editor")
+						.value_name("BOOL")
+						.help("Turn default editor name on or off. (Must have $EDITOR variable set.).")
 						.takes_value(true))
 					.arg(Arg::with_name("shell")
 						.short("s")
 						.long("shell")
 						.value_name("BOOL")
-						.help("Turn default shell name on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn default shell name on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("terminal")
 						.short("t")
 						.long("terminal")
 						.value_name("BOOL")
-						.help("Turn terminal display on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn terminal display on or off. Requires \"term\", and you must be in the same directory as both fetch and term.")
 						.takes_value(true))
 					.arg(Arg::with_name("window_manager")
 						.short("w")
 						.long("window_manager")
 						.value_name("BOOL")
-						.help("Turn window manager name on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn window manager name on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("distro")
 						.short("d")
 						.long("distro")
 						.value_name("BOOL")
-						.help("Turn distro name on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn distro name on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("kernel")
 						.short("k")
 						.long("kernel")
 						.value_name("BOOL")
-						.help("Turn kernel info on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn kernel info on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("uptime")
 						.short("u")
 						.long("uptime")
 						.value_name("BOOL")
-						.help("Turn uptime info on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn uptime info on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("packages")
 						.short("p")
 						.long("packages")
 						.value_name("BOOL")
-						.help("Turn total package count on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn total package count on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("package_counts")
 						.short("P")
 						.long("package_counts")
 						.value_name("BOOL")
-						.help("Turn separate package counts on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn separate package counts on or off.")
 						.takes_value(true))
 					.arg(Arg::with_name("music")
 						.short("m")
@@ -100,12 +106,13 @@ fn main() {
 						.short("l")
 						.long("logo")
 						.value_name("BOOL")
-						.help("Turn the logo (VALLEY) on or off. Supported options are \"true\" and \"false\".")
+						.help("Turn the logo (VALLEY) on or off.")
 						.takes_value(true))
 					.get_matches();
 	let caps = matches.value_of("caps").unwrap_or("true");
 	let user = matches.value_of("user").unwrap_or("true");
 	let ip_address = matches.value_of("ip_address").unwrap_or("true");
+	let editor = matches.value_of("editor").unwrap_or("true");
 	let shell = matches.value_of("shell").unwrap_or("true");
 	let terminal = matches.value_of("terminal").unwrap_or("true");
 	let window_manager = matches.value_of("window_manager").unwrap_or("true");
@@ -117,6 +124,11 @@ fn main() {
 	let music = matches.value_of("music").unwrap_or("no");
 	let logo = matches.value_of("logo").unwrap_or("true");
 	let you = Command::new("/usr/bin/whoami")
+					.output()
+					.expect("failed to execute process");
+	let ed = Command::new("/usr/bin/bash")
+					.arg("-c")
+					.arg("echo $EDITOR")
 					.output()
 					.expect("failed to execute process");
 	let sh = Command::new("/usr/bin/bash")
@@ -167,6 +179,9 @@ fn main() {
 		if ip_address == "true" {
 			table.add_row(row!["IP ADDRESS", "=", String::from_utf8_lossy(&ip.stdout)]);
 		}
+		if editor == "true" {
+			table.add_row(row!["EDITOR", "=", String::from_utf8_lossy(&ed.stdout)]);
+		}
 		if shell == "true" {
 			table.add_row(row!["SHELL", "=", String::from_utf8_lossy(&sh.stdout)]);
 		}
@@ -215,6 +230,9 @@ fn main() {
 		}
 		if ip_address == "true" {
 			table.add_row(row!["ip address", "=", String::from_utf8_lossy(&ip.stdout)]);
+		}
+		if editor == "true" {
+			table.add_row(row!["editor", "=", String::from_utf8_lossy(&ed.stdout)]);
 		}
 		if shell == "true" {
 			table.add_row(row!["shell", "=", String::from_utf8_lossy(&sh.stdout)]);
