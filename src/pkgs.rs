8159f948583ae894ce6e943863b31108460d6348
Kied Llaentenn <32681240+Kiedtl@users.noreply.github.com>
2019-12-19 21:57:46 -0500

    move utility function to pkgs.rs

--- src/pkgs.rs
+++ src/pkgs.rs
@@ -1,4 +1,3 @@
-mod util;
 use crate::util::*;
 use crate::Error;
 use std::vec::Vec;
@@ -37,6 +36,19 @@ impl PkgInfo {
             manager: Vec::new(),
             count:   0,
         }
+    }
+
+    fn count_lines(data: Vec<u8>) -> usize {
+        let mut count: usize = 0;
+
+        // convert srcs from Vec<u8> to String
+        let mut src = "".to_owned();
+        for byte in data {
+            src = format!("{}{}", src, byte as char);
+        }
+
+        let _ = src.split("\n").map(|_| count += 1).collect::<()>();
+        count
     }
 
     pub fn get(&mut self) -> Result<(), Error> {
