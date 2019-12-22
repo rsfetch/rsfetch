c37e623794faff0705df559d59165cddbf7ace18
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-12-22 15:55:17 -0500

    fix: os error 2 on cpu

--- src/cpu.rs
+++ src/cpu.rs
@@ -31,14 +31,22 @@ impl CPUInfo {
 
         // frequency
         let freq_file = "/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq";
-        self.freq = (fs::read_to_string(freq_file).context(CPUErr)?
-            .trim_end().parse::<usize>().unwrap()) / 1000000;
+        if fs::metadata(freq_file).is_ok() {
+            self.freq = (fs::read_to_string(freq_file).context(CPUErr)?
+                .trim_end().parse::<usize>().unwrap()) / 1000000;
+        } else {
+            self.freq = 0;
+        }
 
         Ok(())
     }
 
-    // format it
+    // format it, depending on whether we were able to get the frequency
     pub fn format(&self) -> String {
-        format!("{} ({}) @ {}GHz", self.model, self.cores, self.freq)
+        if self.freq != 0 {
+            format!("{} ({}) @ {}GHz", self.model, self.cores, self.freq)
+        } else {
+            format!("{} ({})", self.model, self.cores)
+        }
     }
 }
