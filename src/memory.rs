cfe22eff8d63d16bab5eb37df516306d0802a767
Kiëd Llaentenn <32681240+kiedtl@users.noreply.github.com>
2019-12-30 13:28:47 -0500

    mem: implement for DragonFlyBSD and FreeBSD

--- src/memory.rs
+++ src/memory.rs
@@ -18,9 +18,9 @@ impl RAMInfo {
 
     pub fn get(&mut self, os: OS) -> Result<()> {
         // for Linux OSs
+        let mut total = 0_u64;
+        let mut used  = 0_u64;
         if os == OS::Linux {
-            let mut total: u64 = 0_u64;
-            let mut used:  u64 = 0_u64;
             let _ = fs::read_to_string("/proc/meminfo")
                 .context(RAMErr)?.split("\n").for_each(|i| {
                     let inf = i.split(":").collect::<Vec<&str>>();
@@ -48,29 +48,60 @@ impl RAMInfo {
             self.total = Some(total / 1024);
             return Ok(());
         } else if os == OS::OpenBSD {
-            let mut total: u64 = 0_u64;
-            let mut used:  u64 = 0_u64;
-
             let mut buffer = String::new();
             Command::new("sysctl").arg("-n").arg("hw.physmem")
                 .output().context(RAMErr)?.stdout.iter()
                 .for_each(|b| buffer.push(*b as char));
-            total = buffer.parse::<u64>();
+            total = buffer.parse::<u64>().unwrap();
 
             // flush buffer
             buffer = "".to_owned();
 
             Command::new("vmstat").output().context(RAMErr)?
                 .stdout.iter().for_each(|b| buffer.push(*b as char));
-            used = buffer.split("\n").last().split(" ").nth(2).parse();
+            used = buffer.split("\n").last().split(" ").nth(2)
+                .parse::<u64>().unwrap();
 
             self.used  = Some(used  / 1024 / 1024);
             self.total = Some(total / 1024 / 1024);
             return Ok(());
-        } else if os == OS::FreeBSD ||
-            os == OS::Other {
+        } else if os == OS::FreeBSD || os == OS::Other {
+            let mut buffer = String::new();
+            Command::new("sysctl").arg("-n").arg("hw.physmem")
+                .output().context(RAMErr)?.stdout.iter()
+                .for_each(|b| buffer.push(*b as char));
+            total = buffer.parse::<u64>().unwrap();
+
+            let pagesize: u64;
+            let inactive: u64;
+            let free:     u64;
+            let cache:    u64;
+            buffer = "";
+
+            Command::new("sysctl").arg("-n")
+                .arg("hw.pagesize")
+                .arg("vm.stats.vm.v_inactive_count")
+                .arg("vm.stats.vm.v_free_count")
+                .arg("vm.stats.vm.v_cache_count")
+                .output().context(RAMErr)?.stdout
+                .iter().for_each(|b| buffer.push(*b as char));
+
+            let info = buffer.split("\n").collect::<Vec<&str>();
+            pagesize = info[0].parse::<u64>();
+            inactive = info[1].parse::<u64>();
+            free     = info[2].parse::<u64>();
+            cache    = info[3].parse::<u64>();
+
+            self.total = total / 1024 / 1024;
+            self.used  = self.total - ((inactive + free + count) * pagesize / 1024 / 1024);
+
             return Ok(());
         } else if os == OS::NetBSD {
+            return Ok(());
+        } else {
+            // leave memory information null,
+            // it will be displayed later
+            // as simply "?MiB / ?MiB"
             return Ok(());
         }
     }
